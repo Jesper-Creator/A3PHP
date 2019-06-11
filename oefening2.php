3858792746e61b36fbaee57d87a59b734b5bf207
Kevin <koster.2001@gmail.com>
2019-06-11 12:54:38 +0200

    code, maar niet werkend(?)

--- oefening2.php
+++ oefening2.php
@@ -3,20 +3,26 @@
   class User{
 
     public $username;
+    public $achternaam;
+    public $geboortedatum;
 
-    public function setUsername($name) {
+    public function setUserinfo($name, $surname, $birth) {
       $this->$username = $name;
+      $this->$achternaam = $surname;
+      $this->$geboortedatum = $birth;
     }
 
-    public function getUsername() {
+    public function getUsernameinfo() {
       return $this->$username;
+      return $this->$achternaam;
+      return $this->$geboortedatum;
     }
   }
 
 // code om de username te 'setten'
 $user = new User();
-$user->setUsername("Kevin");
+$user->setUserinfo("Kevin", "Koster", "7 oktober 2001");
 
-echo $user->getUsername();
+echo $user->getUsernameinfo();
 
  ?>
