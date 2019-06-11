4f447dc2cffe7b79b77fa67b617b566bd20ba9b3
Kevin <koster.2001@gmail.com>
2019-06-11 13:22:04 +0200

    werkend, met alleen voornaam

--- oefening2.php
+++ oefening2.php
@@ -1,28 +1,40 @@
 <?php
 
+
+
   class User{
 
-    public $username;
+    public $voornaam;
     public $achternaam;
     public $geboortedatum;
 
-    public function setUserinfo($name, $surname, $birth) {
-      $this->$username = $name;
-      $this->$achternaam = $surname;
-      $this->$geboortedatum = $birth;
-    }
+      public function setUserinfo($name, $surname, $birthDate)
+      {
+        $this->voornaam = $name;
+        $this->achternaam = $surname;
+        $this->geboortedatum = $birthDate;
+      }
 
-    public function getUsernameinfo() {
-      return $this->$username;
-      return $this->$achternaam;
-      return $this->$geboortedatum;
-    }
+      public function getUserinfo()
+      {
+        return $this->voornaam;
+        return $this->achternaam;
+        return $this->geboortedatum;
+      }
+
   }
 
-// code om de username te 'setten'
-$user = new User();
-$user->setUserinfo("Kevin", "Koster", "7 oktober 2001");
 
-echo $user->getUsernameinfo();
+        //de waarden meegeven aan de set
+        $user = new User();
+        $user->setUserinfo("Kevin", "Koster", "7 oktober 2001");
+
+        echo $user->getUserinfo();
+
+
+
+
+
+
 
  ?>
