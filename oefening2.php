834824db4d0868d8c707440ad6d7ed874460dded
Kevin <koster.2001@gmail.com>
2019-06-11 13:46:05 +0200

    oef3.txt toegevoegd

--- oefening2.php
+++ oefening2.php
@@ -2,34 +2,34 @@
 
 
 
-  class User{
+    class User{
 
-    public $voornaam;
-    public $achternaam;
-    public $geboortedatum;
+      public $voornaam;
+      public $achternaam;
+      public $geboortedatum;
 
-      public function setUserinfo($name, $surname, $birthDate)
-      {
-        $this->voornaam = $name;
-        $this->achternaam = $surname;
-        $this->geboortedatum = $birthDate;
-      }
+        public function setUserinfo($name, $surname, $birthDate)
+        {
+          $this->voornaam = $name;
+          $this->achternaam = $surname;
+          $this->geboortedatum = $birthDate;
+        }
 
-      public function getUserinfo()
-      {
-        return $this->voornaam;
-        return $this->achternaam;
-        return $this->geboortedatum;
-      }
+        public function getUserinfo()
+        {
+          return $this->voornaam;
+          return $this->achternaam;
+          return $this->geboortedatum;
+        }
 
-  }
+    }
 
 
-        //de waarden meegeven aan de set
-        $user = new User();
-        $user->setUserinfo("Kevin", "Koster", "7 oktober 2001");
+          //de waarden meegeven aan de set
+          $user = new User();
+          $user->setUserinfo("Kevin", "Koster", "7 oktober 2001");
 
-        echo $user->getUserinfo();
+          echo $user->getUserinfo();
 
 
 
