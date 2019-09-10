cc179c4de90c13b1fbe21cae0446446b31c34eb0
Kevin <koster.2001@gmail.com>
2019-09-10 09:15:15 +0200

    opdracht 10 klaar

--- opdracht10.php
+++ opdracht10.php
@@ -3,31 +3,37 @@
   //probeer alle code uit inheritance uit.
 
 
-  class User {
-    protected $_username;
+    class User {
+      protected $_username;
 
-      public function __construct($name) {
-        $this->_username = $name;
+        public function __construct($name) {
+          $this->_username = $name;
+        }
+
+        public function getUsername() {
+          return $this->_username;
+        }
+    }
+
+    class Customer extends User {
+      private $_customerId;
+
+      public function __construct($username, $customerId) {
+        $this->_username = $username;
+        $this->_customerId = $customerId;
       }
 
       public function getUsername() {
-        return $this->_username;
+        if ($this->_username == 'Pieter') {
+          return parent::getUsername();
+        }
+        else {
+          return "De gebruikersnaam is:" .$this->_username;
+        }
       }
-  }
-
-  class Customer extends User {
-    private $_customerId;
-
-    public function __construct($username, $customerId) {
-      $this->_username = $username;
-      $this->_customerId = $customerId;
     }
 
-    public function getUsername() {
-      return "De gebruikersnaam is:" .$this->_username;
-    }
-  }
-
-  $customer = new Customer('Pieter', 1);
-  echo $customer->getUsername();
+    $pieter = new Customer('Pieter', 1);
+    $klaas = new Customer('Klaas', 2);
+    echo $pieter->getUsername(). '</br>' .$klaas->getUsername();
  ?>
