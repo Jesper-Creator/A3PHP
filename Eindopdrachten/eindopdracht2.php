1a37255481b8902d3e3c1560f09816f99a338bb8
Kevin <koster.2001@gmail.com>
2019-09-11 11:40:51 +0200

    progressie????

--- Eindopdrachten/eindopdracht2.php
+++ Eindopdrachten/eindopdracht2.php
@@ -2,11 +2,62 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="eindopdr2.css">
     <title>Eindopdracht 2</title>
   </head>
   <body>
 
-    
+    <h1>Memory Game</h1>
+
+    <section class="game">
+      <div class="card">
+        <img class="front-face" src="img/one.svg" alt="One">
+      </div>
+      <div class="card">
+        <img class="front-face" src="img/one.svg" alt="One">
+      </div>
+      <div class="card">
+        <img class="front-face" src="img/two.svg" alt="Two">
+      </div>
+      <div class="card">
+        <img class="front-face" src="img/two.svg" alt="Two">
+      </div>
+      <div class="card">
+        <img class="front-face" src="img/three.svg" alt="Three">
+      </div>
+      <div class="card">
+        <img class="front-face" src="img/three.svg" alt="Three">
+      </div>
+      <div class="card">
+        <img class="front-face" src="img/four.svg" alt="four">
+      </div>
+      <div class="card">
+        <img class="front-face" src="img/four.svg" alt="four">
+        <div class="card">
+          <img class="front-face" src="img/five.svg" alt="five">
+        </div>
+        <div class="card">
+          <img class="front-face" src="img/five.svg" alt="five">
+        </div>
+        <div class="card">
+          <img class="front-face" src="img/six.svg" alt="six">
+        </div>
+        <div class="card">
+          <img class="front-face" src="img/six.svg" alt="six">
+        </div>
+        <div class="card">
+          <img class="front-face" src="img/seven.svg" alt="seven">
+        </div>
+        <div class="card">
+          <img class="front-face" src="img/seven.svg" alt="seven">
+        </div>
+        <div class="card">
+          <img class="front-face" src="img/eight.svg" alt="eight">
+        </div>
+        <div class="card">
+          <img class="front-face" src="img/eight.svg" alt="eight">
+      </div>
+    </section>
 
   </body>
 </html>
