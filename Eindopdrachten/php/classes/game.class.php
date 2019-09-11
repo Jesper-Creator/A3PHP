1dc0b68865ed76abbb97789ce5b5edb80b8e0dc3
JesperBorst <125890@student.drenthecollege.nl>
2019-09-11 16:38:52 +0200

    Comments in de code zodat het makkelijker is te begrijpen.

--- Eindopdrachten/php/classes/game.class.php
+++ Eindopdrachten/php/classes/game.class.php
@@ -1,19 +1,19 @@
 <?php
 class Game {
-
+  //Hier wordt de array aangemaakt.
   private $cards = array();
-
+  //Hier worden de kaarten (2x8 kaarten), in de array gepushed.
   function __construct(){
     for($i=1;$i<9;$i++){
       array_push($this->cards, new Card($i));
       array_push($this->cards, new Card($i));
     }
   }
-
+  //????????????????????????
   public function getCards(){
     return $this->cards;
   }
-
+  //????????????????????????
   public function getCard($index){
     return $this->cards[$index];
   }
