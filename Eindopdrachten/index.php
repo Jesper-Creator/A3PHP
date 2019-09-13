ae9ad78e4504a110b0ee5aa4fa67940377ecfb42
Kevin <koster.2001@gmail.com>
2019-09-13 08:46:46 +0200

    css toegevoegd

--- Eindopdrachten/index.php
+++ Eindopdrachten/index.php
@@ -7,30 +7,34 @@
   </head>
   <body>
 
+    <h1 class="memTxt">Memory Game</h1>
+
+  <div class="main">
     <?php
-      //Deze require verwijst naar de autoload.
-      require_once("php/inc/default.inc.php");
+    //Deze require verwijst naar de autoload.
+    require_once("php/inc/default.inc.php");
 
-      //Start een nieuwe Game
-      //Start nieuw spel als end is aangeklikt
-      if(isset($_GET['end'])){
-        $game = new Game(true);
-      } else {
-        $game = new Game(false);
-      }
-      //klikken kaart verwerken
-      if(isset($_GET['card'])){
-        $game->turnCard($_GET['card']);
-      }
-      //zet kaarten op pagina
-      for($c=0; $c < $game->countCards(); $c++){
-        echo "<a href=\"?card=$c\">" . $game->getCard($c)->getHTML() . "</a>";
-      }
-      //sla huidige game op
-      $game->saveState();
+    //Start een nieuwe Game
+    //Start nieuw spel als end is aangeklikt
+    if(isset($_GET['end'])){
+      $game = new Game(true);
+    } else {
+      $game = new Game(false);
+    }
+    //klikken kaart verwerken
+    if(isset($_GET['card'])){
+      $game->turnCard($_GET['card']);
+    }
+    //zet kaarten op pagina
+    for($c=0; $c < $game->countCards(); $c++){
+      echo "<a href=\"?card=$c\">" . $game->getCard($c)->getHTML() . "</a>";
+    }
+    //sla huidige game op
+    $game->saveState();
     ?>
+  </div>
 
-    <a href="?end"><button type="button" name="button">New game</button></a>
+    <a href="?end"><button type="button" name="button">Restart</button></a>
 
   </body>
 </html>
